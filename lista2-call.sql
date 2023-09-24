b023ea9d3cbcbbe4d2c3f0c626ef6b50d538320e
Jean Mendes <jeanmds7809@gmail.com>
2023-09-24 15:32:58 -0300

    exercicio 3

--- lista2-call.sql
+++ lista2-call.sql
@@ -5,3 +5,7 @@ CALL sp_ListarAutores();
 -- 2
 
 CALL sp_LivrosPorCategoria('Ficção Científica');
+
+-- 3
+
+CALL sp_ContarLivrosPorCategoria('Ciência');
--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -26,3 +26,20 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+-- 3
+
+DELIMITER //
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN CategoriaE varchar(100))
+BEGIN
+	WITH Selet_id AS (
+		select (select Categoria_ID from categoria where nome = CategoriaE) as id_selecionado
+    )
+    
+    Select Categoria_ID, COUNT(Categoria_ID) as qtd_livros
+    FROM livro
+    INNER JOIN Selet_id  on id_selecionado = categoria_id
+    GROUP BY Categoria_ID;
+END;
+//
+DELIMITER ;
